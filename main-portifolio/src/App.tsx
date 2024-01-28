c43ce23009d4a9e15ffd62e5cbd9909b71600e66
RafaZeero <rafaa99@gmail.com>
2024-01-28 19:19:56 -0300

    Add rewrite rule for contas-casa project

--- main-portifolio/src/App.tsx
+++ main-portifolio/src/App.tsx
@@ -5,6 +5,8 @@ import { router } from './routes/root';
 function App() {
   return (
     <>
+      {/* <a href='https://contas-casa.vercel.app/'>contas casa</a> */}
+      <a href='/projetos/contas-casa'>contas casa</a>
       <RouterProvider router={router} />
     </>
   );
