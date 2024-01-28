f7015adaab5e42c940db6890a6f5c757a290fce5
RafaZeero <rafaa99@gmail.com>
2024-01-28 20:07:06 -0300

    remove router provider

--- main-portifolio/src/App.tsx
+++ main-portifolio/src/App.tsx
@@ -1,13 +1,13 @@
-import { RouterProvider } from 'react-router-dom';
+// import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import { router } from './routes/root';
+// import { router } from './routes/root';
 
 function App() {
   return (
     <>
       {/* <a href='https://contas-casa.vercel.app/'>contas casa</a> */}
-      <a href='/projetos/contas-casa'>contas casa</a>
-      <RouterProvider router={router} />
+      <a href='/contas-casa'>contas casa</a>
+      {/* <RouterProvider router={router} /> */}
     </>
   );
 }
